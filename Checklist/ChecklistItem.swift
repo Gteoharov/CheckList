6786ef5251c45d8a27aeb8ec5fe051cfcfb3e9f1
Gteoharov <georgia.teoharov@gmail.com>
2019-06-28 01:37:21 +0300

    refactoring controller

--- Checklist/ChecklistItem.swift
+++ Checklist/ChecklistItem.swift
@@ -15,4 +15,8 @@ class ChecklistItem {
     var text = ""
     var checked = false
     
+    
+    func toggleChecked() {
+        checked = !checked
+    }
 }
--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -16,81 +16,47 @@ class ChecklistViewController: UITableViewController {
         todoList = TodoList()
         super.init(coder: aDecoder)
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return todoList.todos.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        
-        if let label = cell.viewWithTag(1000) as? UILabel {
-            if indexPath.row == 0 {
-                label.text = todoList.todos[0].text
-            } else if indexPath.row == 1 {
-                label.text = todoList.todos[1].text
-            } else if indexPath.row == 2 {
-                label.text = todoList.todos[2].text
-            } else if indexPath.row == 3 {
-                label.text = todoList.todos[3].text
-            } else if indexPath.row == 4 {
-                label.text = todoList.todos[4].text
-            }
-        }
-        configureCheckmark(for: cell, at: indexPath)
+        let item = todoList.todos[indexPath.row]
+        configureText(for: cell, with: item)
+        configureCheckmark(for: cell, with: item)
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let cell = tableView.cellForRow(at: indexPath) {
-            configureCheckmark(for: cell, at: indexPath)
+            let item = todoList.todos[indexPath.row]
+            configureCheckmark(for: cell, with: item)
             tableView.deselectRow(at: indexPath, animated: true)
         }
     }
     
-    func configureCheckmark(for cell: UITableViewCell, at indexPath: IndexPath) {
-        if indexPath.row == 0 {
-            if todoList.todos[0].checked {
-                cell.accessoryType = .none
-            } else {
-                cell.accessoryType = .checkmark
-            }
-            todoList.todos[0].checked = !todoList.todos[0].checked
-        } else if indexPath.row == 1 {
-            if todoList.todos[1].checked {
-                cell.accessoryType = .none
-            } else {
-                cell.accessoryType = .checkmark
-            }
-            todoList.todos[1].checked = !todoList.todos[1].checked
-        } else if indexPath.row == 2 {
-            if todoList.todos[2].checked {
-                cell.accessoryType = .none
-            } else {
-                cell.accessoryType = .checkmark
-            }
-            todoList.todos[2].checked = !todoList.todos[2].checked
-        } else if indexPath.row == 3 {
-            if todoList.todos[3].checked {
-                cell.accessoryType = .none
-            } else {
-                cell.accessoryType = .checkmark
-            }
-            todoList.todos[3].checked = !todoList.todos[3].checked
-        } else if indexPath.row == 4 {
-            if todoList.todos[4].checked {
-                cell.accessoryType = .none
-            } else {
-                cell.accessoryType = .checkmark
-            }
-            todoList.todos[4].checked = !todoList.todos[4].checked
+    func configureText(for cell: UITableViewCell, with item: ChecklistItem) {
+        if let label = cell.viewWithTag(1000) as? UILabel {
+            label.text = item.text
         }
+    }
+    
+    func configureCheckmark(for cell: UITableViewCell, with item: ChecklistItem) {
+        
+        if item.checked {
+            cell.accessoryType = .none
+        } else {
+            cell.accessoryType = .checkmark
+        }
+        item.toggleChecked()
     }
 }
 
