6956c4cb79c87255c77fc814bb139b52eb1d1b39
Gteoharov <georgia.teoharov@gmail.com>
2019-06-27 16:09:36 +0300

    Initial commit

---
+++ Checklist/ChecklistViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Georgi Teoharov on 27.06.19.
+//  Copyright © 2019 Georgi Teoharov. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
+
