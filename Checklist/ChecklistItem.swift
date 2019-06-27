e1121454500c751c991143b754b27731378e0e87
Gteoharov <georgia.teoharov@gmail.com>
2019-06-27 19:04:15 +0300

    model create

---
+++ Checklist/ChecklistItem.swift
@@ -0,0 +1,18 @@
+//
+//  ChecklistItem.swift
+//  Checklist
+//
+//  Created by Georgi Teoharov on 27.06.19.
+//  Copyright © 2019 Georgi Teoharov. All rights reserved.
+//
+
+import Foundation
+
+
+
+class ChecklistItem {
+    
+    var text = ""
+    var checked = false
+    
+}
